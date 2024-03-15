c059168923a4f46cc2414abc6116e234fcd83ca3
Štef <stjepan.salopek@hotmail.com>
2024-03-15 13:25:35 +0100

    Update Properties file

--- BusinessDiary/app/src/main/java/com/github/shtef21/businessdiary/logic/AppDb.kt
+++ BusinessDiary/app/src/main/java/com/github/shtef21/businessdiary/logic/AppDb.kt
@@ -1,6 +1,6 @@
 package com.github.shtef21.businessdiary.logic
 
-import com.github.shtef21.businessdiary.AppProperties.logic.dbUrl
+import com.github.shtef21.businessdiary.logic.AppProperties.dbUrl
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
 
